5fe3e3fb895cc14dd5b736fa4a4627142f37b48e
rob <robert_rivmtz@hotmail.com>
2015-04-26 17:07:02 -0500

    correcion disparar

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/ThirdPersonController.js
@@ -6,7 +6,7 @@ public var idleAnimation : AnimationClip;
 public var walkAnimation : AnimationClip;
 public var runAnimation : AnimationClip;
 public var jumpPoseAnimation : AnimationClip;
-
+var speed = 1000.0;
 public var walkMaxAnimationSpeed : float = 0.75;
 public var trotMaxAnimationSpeed : float = 1.0;
 public var runMaxAnimationSpeed : float = 1.0;
@@ -372,6 +372,28 @@ function Update() {
 			transform.rotation = Quaternion.LookRotation(xzMove);
 		}
 	}	*/
+	    // Generate a plane that intersects the transform's position with an upwards normal.
+    var playerPlane = new Plane(Vector3.up, transform.position);
+
+    // Generate a ray from the cursor position
+    var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+ 
+    // Determine the point where the cursor ray intersects the plane.
+    // This will be the point that the object must look towards to be looking at the mouse.
+    // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+    //   then find the point along that ray that meets that distance.  This will be the point
+    //   to look at.
+    var hitdist = 0.0;
+    // If the ray is parallel to the plane, Raycast will return false.
+    if (playerPlane.Raycast (ray, hitdist)) {
+        // Get the point along the ray that hits the calculated distance.
+        var targetPoint = ray.GetPoint(hitdist);
+ 
+        // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+        var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+        // Smoothly rotate towards the target point.
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+    }
 	
 	// We are in jump mode but just became grounded
 	if (IsGrounded())
