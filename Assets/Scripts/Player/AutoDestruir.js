7048f3223b2e0e1c5bc66cecebfe6d3ac03037f4
luissifu@laptop <luis.e.sifuentes@gmail.com>
2015-05-13 13:44:17 -0500

    updated laser prefab, added particles and light

--- Assets/Scripts/Player/AutoDestruir.js
+++ Assets/Scripts/Player/AutoDestruir.js
@@ -1,3 +1,19 @@
-﻿function Start () {
+﻿public var particles_pfb : GameObject;
+
+function Start () {
 	Destroy(this.gameObject, 1);
+}
+
+function Update() {
+	var fwd = transform.TransformDirection (Vector3.forward);
+	var hit : RaycastHit;
+	if (Physics.Raycast(transform.position, fwd, hit, 2))
+	{
+		if (hit.collider.tag != "Player")
+		{
+			var particle : GameObject = Instantiate(particles_pfb, transform.position, Quaternion.identity);
+			particle.transform.Rotate(-90,0,0);
+			Destroy(this.gameObject);
+		}
 	}
+}
