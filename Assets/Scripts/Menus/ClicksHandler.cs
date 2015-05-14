a2056d0579e6d8d14fb565155c4773315dc09f50
rob <robert_rivmtz@hotmail.com>
2015-05-14 16:40:41 -0500

    menus

---
+++ Assets/Scripts/Menus/ClicksHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClicksHandler : MonoBehaviour {
+
+	public void onCommand(string str)
+	{
+		if(str.Equals("Menu"))
+		{
+
+			Application.LoadLevel("MainMenu");
+			
+		}
+		
+		else if(str.Equals("Game"))
+		{
+			Application.LoadLevel("Main copia");
+			
+		}
+	
+		
+		
+	}
+}
