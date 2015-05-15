d57eae5309e3dc8eaf0ef8450b9ffc7ba5fe6d8f
rob <robert_rivmtz@hotmail.com>
2015-05-15 13:05:32 -0500

    pausa

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	public GameObject canvas;
+	public Camera gameCamera;
+	public GameObject planet;
+	public GameObject fullGame;
+	public Camera pauseCamera;
+	private bool isPaused;
+	// Use this for initialization
+	void Start () {
+		canvas.SetActive (false);
+		planet.SetActive (false);
+		fullGame.SetActive (true);
+		gameCamera.enabled = true;
+		pauseCamera.enabled = false;
+		isPaused = false;
+
+	}
+
+	public void changeCamera(){
+		isPaused = !isPaused;
+		gameCamera.enabled = isPaused;
+		pauseCamera.enabled = !isPaused;
+		fullGame.SetActive(isPaused);
+		canvas.SetActive(!isPaused);
+		planet.SetActive(!isPaused);
+		}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.P)) {
+			changeCamera();
+		}
+	}
+}
