cafafca51598ecad8a20470792c256188483a89c
luissifu@laptop <luis.e.sifuentes@gmail.com>
2015-05-15 14:12:21 -0500

    added enemy bullet pfb

---
+++ Assets/EnemyBullet.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public var damage : int = 10;
+
+function Update() {
+	var fwd = transform.TransformDirection (Vector3.forward);
+	var hit : RaycastHit;
+	if (Physics.Raycast(transform.position, fwd, hit, 2))
+	{
+		var col = hit.collider;
+		Debug.Log(col.name);
+		
+		if (col.tag != "EnemySphere" && col.tag != "Enemy")
+		{
+			
+			if (col.tag == "Player")
+			{
+				col.GetComponent(PlayerHealth).TakeDamage(damage);	
+			}
+			
+			Destroy(this.gameObject);
+		}
+	}
+}
